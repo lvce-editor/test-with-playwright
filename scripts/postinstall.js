a8f031c0261dd3a615307ca61a96e27a98745ad6
Le Vivilet <levivlet@pm.me>
2022-08-19 13:53:57 +0200

    fix postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -15,7 +15,7 @@ const exec = async (command, args, options) => {
 const main = async () => {
   const __dirname = dirname(fileURLToPath(import.meta.url))
   const root = join(__dirname, '..')
-  const fixturePath = join(root, 'packages', 'e2e', 'test', 'fixtures')
+  const fixturePath = join(root, 'packages', 'e2e', 'fixtures')
   const fixtureNames = await readdir(fixturePath)
   for (const fixtureName of fixtureNames) {
     await exec('npm', ['ci', '--ignore-scripts'], {
