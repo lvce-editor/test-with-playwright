6d5206b5fd69e390d5a55ee4e34583f6b27158b5
Le Vivilet <levivlet@pm.me>
2022-09-06 10:46:03 +0200

    increase timeout for completion provider e2e test

--- packages/e2e/test/sample.completion-provider.test.js
+++ packages/e2e/test/sample.completion-provider.test.js
@@ -1,5 +1,7 @@
 import { runFixture } from './_runFixture.js'
 
+const timeout = 30_000
+
 test('sample.completion-provider', async () => {
   const { stdout, stderr, exitCode } = await runFixture(
     'sample.completion-provider'
@@ -7,4 +9,4 @@ test('sample.completion-provider', async () => {
   expect(exitCode).toBe(0)
   expect(stdout).toMatch(/1 tests passed in \d+(\.\d+)?ms/)
   // expect(stderr).toBe('')
-})
+}, timeout)
