007193e4b2065aa19b7eabd8afc17923c0a7fe4c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-05-24 19:28:43 +0200

    fix: improve error handling when no test files are found (#52)

--- packages/test-with-playwright/src/parts/GetTestFiles/GetTestFiles.js
+++ packages/test-with-playwright/src/parts/GetTestFiles/GetTestFiles.js
@@ -1,12 +1,21 @@
-import { readdirSync } from 'node:fs'
+import { readdir } from 'node:fs/promises'
 import { join } from 'node:path'
+import * as ErrorCodes from '../ErrorCodes/ErrorCodes.js'
 import * as IsTestFile from '../IsTestFile/IsTestFile.js'
+import VError from 'verror'
+import { NoTestFilesFoundError } from '../NoTestFilesFoundError/NoTestFIlesFoundError.js'
 
 /**
  * @param {string} root
  */
 export const getTestFiles = async (root) => {
-  return readdirSync(root)
-    .filter(IsTestFile.isTestFile)
-    .map((x) => join(root, x))
+  try {
+    const dirents = await readdir(root)
+    return dirents.filter(IsTestFile.isTestFile).map((x) => join(root, x))
+  } catch (error) {
+    if (error && error.code === ErrorCodes.ENOENT) {
+      throw new NoTestFilesFoundError(root)
+    }
+    throw new VError(error, `Failed to get test files`)
+  }
 }
