8485c705b2c366db64ea073294f39da05ce5155d
Le Vivilet <levivlet@pm.me>
2023-05-27 17:57:55 +0200

    improve final result message formatting

---
+++ packages/test-with-playwright/src/parts/GetFinalResultMessage/GetFinalResultMessage.js
@@ -0,0 +1,27 @@
+export const getFinalResultMessage = (passed, skipped, failed, duration) => {
+  if (passed === 0 && skipped === 0 && failed === 0) {
+    return `no tests found`
+  }
+  if (passed === 0 && skipped === 0 && failed === 1) {
+    return `${passed} test failed in ${duration}ms`
+  }
+  if (passed === 0 && skipped === 1 && failed === 0) {
+    return `${skipped} test skipped in ${duration}ms`
+  }
+  if (passed === 0 && skipped === 1 && failed === 1) {
+    return `${skipped} test skipped, ${failed} test failed in ${duration}ms`
+  }
+  if (passed === 1 && skipped === 0 && failed === 0) {
+    return `${passed} test passed in ${duration}ms`
+  }
+  if (passed === 1 && skipped === 0 && failed === 1) {
+    return `${passed} test passed, ${failed} test failed in ${duration}ms`
+  }
+  if (passed === 1 && skipped === 1 && failed === 0) {
+    return `${passed} test passed, ${skipped} test skipped in ${duration}ms`
+  }
+  if (passed === 1 && skipped === 1 && failed === 1) {
+    return `${passed} test passed, ${skipped} test skipped, ${failed} test failed in ${duration}ms`
+  }
+  return `${passed} tests passed, ${skipped} test skipped, ${failed} test failed in ${duration}ms`
+}
