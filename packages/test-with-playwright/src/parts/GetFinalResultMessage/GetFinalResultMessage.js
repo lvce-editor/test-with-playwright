fc026f17b2738031c4bf35fd8e159c1a01ce8af3
Le Vivilet <levivlet@pm.me>
2023-05-27 18:00:04 +0200

    improve final message

--- packages/test-with-playwright/src/parts/GetFinalResultMessage/GetFinalResultMessage.js
+++ packages/test-with-playwright/src/parts/GetFinalResultMessage/GetFinalResultMessage.js
@@ -23,5 +23,8 @@ export const getFinalResultMessage = (passed, skipped, failed, duration) => {
   if (passed === 1 && skipped === 1 && failed === 1) {
     return `${passed} test passed, ${skipped} test skipped, ${failed} test failed in ${duration}ms`
   }
-  return `${passed} tests passed, ${skipped} test skipped, ${failed} test failed in ${duration}ms`
+  if (passed > 1 && skipped === 0 && failed === 0) {
+    return `${passed} tests passed in ${duration}ms`
+  }
+  return `${passed} tests passed, ${skipped} tests skipped, ${failed} tests failed in ${duration}ms`
 }
