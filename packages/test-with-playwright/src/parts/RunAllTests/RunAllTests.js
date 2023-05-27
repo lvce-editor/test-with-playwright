481d29304e5599c1fe7ae8110b91f210157c8260
Le Vivilet <levivlet@pm.me>
2023-05-27 17:06:58 +0200

    remove console log

--- packages/test-with-playwright/src/parts/RunAllTests/RunAllTests.js
+++ packages/test-with-playwright/src/parts/RunAllTests/RunAllTests.js
@@ -1,16 +1,15 @@
 import * as GetTestWorkerPath from '../GetTestWorkerPath/GetTestWorkerPath.js'
+import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 import * as IpcParent from '../IpcParent/IpcParent.js'
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
 import * as JsonRpc from '../JsonRpc/JsonRpc.js'
 import * as TestWorkerCommandType from '../TestWorkerCommandType/TestWorkerCommandType.js'
-import * as HandleIpc from '../HandleIpc/HandleIpc.js'
 
 /**
  *
  * @param {{extensionPath:string, testPath:string, cwd:string, headless:boolean, timeout:number}} param0
  */
 export const runAllTests = async ({ extensionPath, testPath, cwd, headless, timeout }) => {
-  console.log('start')
   const path = GetTestWorkerPath.getTestWorkerPath()
   const ipc = await IpcParent.create({
     method: IpcParentType.NodeWorker,
@@ -18,8 +17,6 @@ export const runAllTests = async ({ extensionPath, testPath, cwd, headless, time
     argv: [],
   })
   HandleIpc.handleIpc(ipc)
-  console.time('run tests')
   await JsonRpc.invoke(ipc, TestWorkerCommandType.RunAllTests, extensionPath, testPath, cwd, headless, timeout)
-  console.timeEnd('run tests')
   ipc.dispose()
 }
