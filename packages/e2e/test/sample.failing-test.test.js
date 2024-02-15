d07d2d154661d23d77f7a9513a4f4e83a71e0604
Le Vivilet <levivlet@pm.me>
2024-02-15 22:18:37 +0100

    fix: set correct exit code

--- packages/e2e/test/sample.failing-test.test.js
+++ packages/e2e/test/sample.failing-test.test.js
@@ -4,5 +4,5 @@ test('sample.failing-test', async () => {
   const { stdout, stderr, exitCode } = await runFixture('sample.failing-test')
   expect(exitCode).toBe(0)
   expect(stdout).toMatch(/1 test failed in \d+(\.\d+?)ms/)
-  // expect(stderr).toBe('')
+  expect(exitCode).toBe(1)
 })
