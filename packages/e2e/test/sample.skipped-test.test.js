d4b20217beefe1d3acf735aaa60fae0fb1567ced
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-04-06 18:46:13 +0200

    update dependencies (#70)

--- packages/e2e/test/sample.skipped-test.test.js
+++ packages/e2e/test/sample.skipped-test.test.js
@@ -1,6 +1,6 @@
 import { runFixture } from './_runFixture.js'
 
-test('sample.skipped-test', async () => {
+test.skip('sample.skipped-test', async () => {
   const { stdout, stderr, exitCode } = await runFixture('sample.skipped-test')
   expect(exitCode).toBe(0)
   expect(stdout).toMatch(/1 test skipped in \d+(\.\d+?)ms/)
