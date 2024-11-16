cbb9913434cbb824d37a2d28430bf485ba00a849
Le Vivilet <levivlet@pm.me>
2024-11-16 12:56:06 +0100

    feature: use command npm package

--- packages/test-with-playwright/src/parts/Main/Main.js
+++ packages/test-with-playwright/src/parts/Main/Main.js
@@ -1,11 +1,11 @@
+import * as Command from '../Command/Command.js'
+import * as CommandMap from '../CommandMap/CommandMap.js'
 import * as HandleCliArgs from '../HandleCliArgs/HandleCliArgs.js'
 import * as Process from '../Process/Process.js'
 import * as ProcessListeners from '../ProcessListeners/ProcessListeners.js'
-import * as CommandState from '../CommandState/CommandState.js'
-import * as CommandMap from '../CommandMap/CommandMap.js'
 
 export const main = () => {
   Process.on('uncaughtExceptionMonitor', ProcessListeners.handleUncaughtExceptionMonitor)
-  CommandState.registerCommands(CommandMap.commandMap)
+  Command.register(CommandMap.commandMap)
   HandleCliArgs.handleCliArgs({ argv: Process.argv, env: Process.env })
 }
