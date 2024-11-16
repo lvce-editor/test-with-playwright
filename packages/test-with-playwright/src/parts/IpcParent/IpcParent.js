b3c2a5286c0b9abf94692e2c02cd2455d7f1091f
Le Vivilet <levivlet@pm.me>
2024-11-16 12:57:58 +0100

    feature: remove unused await

--- packages/test-with-playwright/src/parts/IpcParent/IpcParent.js
+++ packages/test-with-playwright/src/parts/IpcParent/IpcParent.js
@@ -1,7 +1,7 @@
 import * as IpcParentModule from '../IpcParentModule/IpcParentModule.js'
 
 export const create = async ({ method, ...options }) => {
-  const module = await IpcParentModule.getModule(method)
+  const module = IpcParentModule.getModule(method)
   // @ts-ignore
   const rawIpc = await module.create(options)
   const ipc = module.wrap(rawIpc)
