7351badc614a914142da18a233bba8756b61298f
Le Vivilet <levivlet@pm.me>
2024-11-16 10:59:58 +0100

    feature: try multiple server paths

--- packages/test-with-playwright-worker/src/parts/GetServerPath/GetServerPath.js
+++ packages/test-with-playwright-worker/src/parts/GetServerPath/GetServerPath.js
@@ -1,4 +1,18 @@
+import { join } from 'path'
+
 export const getServerPath = async () => {
-  const { serverPath } = await import('@lvce-editor/server')
-  return serverPath
+  const toTry = [
+    '@lvce-editor/server',
+    join(process.cwd(), '..', 'server', 'node_modules', '@lvce-editor', 'server', 'index.js'),
+  ]
+
+  for (const path of toTry) {
+    try {
+      const { serverPath } = await import(path)
+      return serverPath
+    } catch (error) {
+      // ignore
+    }
+  }
+  throw new Error(`[test-with-playwright] server path not found`)
 }
