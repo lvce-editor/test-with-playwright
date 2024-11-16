e0bef771d1446fec4f7cf53acbe55987253af463
Le Vivilet <levivlet@pm.me>
2024-11-16 12:23:50 +0100

    feature: use ipc npm package

--- packages/test-with-playwright-worker/src/parts/HandleIpc/HandleIpc.js
+++ packages/test-with-playwright-worker/src/parts/HandleIpc/HandleIpc.js
@@ -1,13 +1,5 @@
-import * as Assert from '../Assert/Assert.js'
-import * as GetResponse from '../GetResponse/GetResponse.js'
+import * as HandleMessage from '../HandleMessage/HandleMessage.js'
 
 export const handleIpc = (ipc) => {
-  Assert.object(ipc)
-  const handleMessage = async (message) => {
-    const response = await GetResponse.getResponse(message, ipc)
-    if (response) {
-      ipc.send(response)
-    }
-  }
-  ipc.on('message', handleMessage)
+  ipc.addEventListener('message', HandleMessage.handleMessage)
 }
