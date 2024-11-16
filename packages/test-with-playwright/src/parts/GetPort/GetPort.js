931d60c838c545c66fa5f3bbda741b0e8427414a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-16 12:52:11 +0100

    feature: remove unused code (#97)

--- packages/test-with-playwright/src/parts/GetPort/GetPort.js
+++
@@ -1,5 +0,0 @@
-import _getPort from 'get-port'
-
-export const getPort = () => {
-  return _getPort()
-}
--- packages/test-with-playwright/src/parts/GetTests/GetTests.js
+++
@@ -1,9 +0,0 @@
-import { readdir } from 'fs/promises'
-
-/**
- * @param {string} testSrc
- */
-export const getTests = async (testSrc) => {
-  const dirents = await readdir(testSrc)
-  return dirents
-}
