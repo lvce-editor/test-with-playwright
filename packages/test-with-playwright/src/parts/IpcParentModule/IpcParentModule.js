3ca166cdbac652f5c8be440a140fca4d2c078ae2
Le Vivilet <levivlet@pm.me>
2024-11-16 12:39:00 +0100

    use

--- packages/test-with-playwright/src/parts/IpcParentModule/IpcParentModule.js
+++ packages/test-with-playwright/src/parts/IpcParentModule/IpcParentModule.js
@@ -1,9 +1,10 @@
 import * as IpcParentType from '../IpcParentType/IpcParentType.js'
+import { IpcParentWithNodeWorker } from '@lvce-editor/ipc'
 
 export const getModule = (method) => {
   switch (method) {
     case IpcParentType.NodeWorker:
-      return import('../IpcParentWithNodeWorker/IpcParentWithNodeWorker.js')
+      return IpcParentWithNodeWorker
     default:
       throw new Error('unexpected ipc type')
   }
