918ca8277ee563ef92251b2f6d9d1ccc7babfd71
Le Vivilet <levivlet@pm.me>
2025-01-18 00:11:39 +0100

    fix: api types location

--- packages/build/src/generateApiTypes.js
+++ packages/build/src/generateApiTypes.js
@@ -4,6 +4,6 @@ import { root } from './root.js'
 
 export const generateApiTypes = async () => {
   const typesPath = join(root, 'packages', 'build', 'node_modules', '@lvce-editor', 'test-worker', 'dist', 'api.d.ts')
-  const outPath = join(root, '.tmp', 'test-with-playwright', 'api.d.ts')
+  const outPath = join(root, 'dist', 'test-with-playwright', 'api.d.ts')
   await cp(typesPath, outPath)
 }
