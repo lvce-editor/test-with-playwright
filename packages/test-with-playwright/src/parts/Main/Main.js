ff6e6e6832ae3cd96e8b5875cf81c28d6d3f7431
Le Vivilet <levivilet@pm.me>
2025-09-06 19:51:19 +0200

    feature: use await

--- packages/test-with-playwright/src/parts/Main/Main.js
+++ packages/test-with-playwright/src/parts/Main/Main.js
@@ -4,8 +4,8 @@ import * as HandleCliArgs from '../HandleCliArgs/HandleCliArgs.js'
 import * as Process from '../Process/Process.js'
 import * as ProcessListeners from '../ProcessListeners/ProcessListeners.js'
 
-export const main = () => {
+export const main = async () => {
   Process.on('uncaughtExceptionMonitor', ProcessListeners.handleUncaughtExceptionMonitor)
   Command.register(CommandMap.commandMap)
-  HandleCliArgs.handleCliArgs({ argv: Process.argv, env: Process.env })
+  await HandleCliArgs.handleCliArgs({ argv: Process.argv, env: Process.env })
 }
