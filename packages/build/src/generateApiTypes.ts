5e5d1f6000441d183342384c43d9e5cb5b9c9e67
Le Vivilet <levivilet@pm.me>
2025-09-08 11:51:27 +0200

    feature: move build to typescript (#273)
    * feature: move build to typescript
    
    * lock

--- packages/build/src/generateApiTypes.ts
+++ packages/build/src/generateApiTypes.ts
@@ -1,8 +1,8 @@
 import { writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
-import { root } from './root.js'
+import { root } from './root.ts'
 
-export const generateApiTypes = async () => {
+export const generateApiTypes = async (): Promise<void> => {
   const typesContent = `export * from '@lvce-editor/test-worker'`
   const outPath = join(root, 'dist', 'test-with-playwright', 'api.d.ts')
   await writeFile(outPath, typesContent)
