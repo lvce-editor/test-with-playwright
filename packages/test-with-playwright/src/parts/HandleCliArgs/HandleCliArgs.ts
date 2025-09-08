efae756645538495fe6b57dc0e3541e311d1835d
Le Vivilet <levivilet@pm.me>
2025-09-08 11:40:31 +0200

    feature: move cli to typescript

---
+++ packages/test-with-playwright/src/parts/HandleCliArgs/HandleCliArgs.ts
@@ -0,0 +1,31 @@
+import * as GetOptions from '../GetOptions/GetOptions.ts'
+import * as RunAllTests from '../RunAllTests/RunAllTests.ts'
+import * as GetTestWorkerPath from '../GetTestWorkerPath/GetTestWorkerPath.ts'
+
+interface HandleCliArgsParams {
+  argv: string[]
+  env: NodeJS.ProcessEnv
+  commandMap: any
+}
+
+export const handleCliArgs = async ({ argv, env, commandMap }: HandleCliArgsParams): Promise<void> => {
+  const cwd = process.cwd()
+  const options = GetOptions.getOptions({ argv, env })
+  const onlyExtension = options.onlyExtension
+  const testPath = options.testPath
+  const headless = options.headless
+  const timeout = 30_000
+  const testWorkerPath = GetTestWorkerPath.getTestWorkerPath()
+
+  // TODO
+  // console.log({ argv, env })
+  await RunAllTests.runAllTests({
+    onlyExtension,
+    testPath,
+    cwd,
+    headless,
+    timeout,
+    commandMap,
+    testWorkerPath,
+  })
+}
