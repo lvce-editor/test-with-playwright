4fc36d3c29e4062dfee1ab4842ef796613dae8b9
Le Vivilet <levivilet@pm.me>
2025-09-08 14:49:09 +0200

    feature: add additional server path to try (#287)

--- packages/test-with-playwright-worker/src/parts/GetPossibleServerPaths/GetPossibleServerPaths.js
+++ packages/test-with-playwright-worker/src/parts/GetPossibleServerPaths/GetPossibleServerPaths.js
@@ -6,6 +6,9 @@ export const getPossibleServerPaths = () => {
     '@lvce-editor/server',
     pathToFileURL(join(process.cwd(), '..', 'server', 'node_modules', '@lvce-editor', 'server', 'index.js')).toString(),
     pathToFileURL(join(process.cwd(), '..', 'build', 'node_modules', '@lvce-editor', 'server', 'index.js')).toString(),
+    pathToFileURL(
+      join(process.cwd(), '..', '..', '..', 'packages', 'server', 'node_modules', '@lvce-editor', 'server', 'index.js'),
+    ).toString(),
   ]
   return toTry
 }
