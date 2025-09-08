8268521345e3b984f0c9d2564ee80ba3fa31609c
Le Vivilet <levivilet@pm.me>
2025-09-08 14:50:55 +0200

    fix

--- packages/test-with-playwright/src/parts/GetTestWorkerPath/GetTestWorkerPath.ts
+++ packages/test-with-playwright/src/parts/GetTestWorkerPath/GetTestWorkerPath.ts
@@ -2,5 +2,5 @@ import { join } from 'node:path'
 import * as Root from '../Root/Root.ts'
 
 export const getTestWorkerPath = (): string => {
-  return join(Root.root, 'packages', 'test-with-playwright-worker', 'src', 'workerMain.js')
+  return join(Root.root, 'packages', 'test-with-playwright-worker', 'src', 'workerMain.ts')
 }
