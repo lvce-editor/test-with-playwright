3c4efc6375fb301bcab30064de4e970112ec19eb
Le Vivilet <levivilet@pm.me>
2025-09-08 13:45:42 +0200

    remove unused files

--- packages/test-with-playwright/src/parts/GetTestWorkerPath/GetTestWorkerPath.js
+++
@@ -1,6 +0,0 @@
-import { join } from 'node:path'
-import * as Root from '../Root/Root.ts'
-
-export const getTestWorkerPath = (): string => {
-  return join(Root.root, 'packages', 'test-with-playwright-worker', 'src', 'workerMain.js')
-}
