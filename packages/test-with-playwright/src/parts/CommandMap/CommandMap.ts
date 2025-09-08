68ec4fa7be623087fae6eb7919e7b923a77f6ed8
Le Vivilet <levivilet@pm.me>
2025-09-08 13:51:55 +0200

    tsc

--- packages/test-with-playwright/src/parts/CommandMap/CommandMap.ts
+++ packages/test-with-playwright/src/parts/CommandMap/CommandMap.ts
@@ -2,7 +2,7 @@ import * as CliCommandType from '../CliCommandType/CliCommandType.ts'
 import * as HandleFinalResult from '../HandleFinalResult/HandleFinalResult.ts'
 import * as HandleResult from '../HandleResult/HandleResult.ts'
 
-export const commandMap = {
+export const commandMap: any = {
   [CliCommandType.HandleResult]: HandleResult.handleResult,
   [CliCommandType.HandleFinalResult]: HandleFinalResult.handleFinalResult,
 }
