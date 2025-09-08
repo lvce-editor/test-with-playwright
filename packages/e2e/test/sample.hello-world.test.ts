bd7036e089645fb9f3976a9208b01445d5a74c8c
Le Vivilet <levivilet@pm.me>
2025-09-08 13:24:00 +0200

    feature: move e2e to typescript (#279)

--- packages/e2e/test/sample.hello-world.test.ts
+++ packages/e2e/test/sample.hello-world.test.ts
@@ -1,6 +1,7 @@
-import { runFixture } from './_runFixture.js'
+import { test, expect } from '@jest/globals'
+import { runFixture } from './_runFixture.ts'
 
-test('sample.hello-world', async () => {
+test('sample.hello-world', async (): Promise<void> => {
   const { stdout, exitCode } = await runFixture('sample.hello-world')
   expect(exitCode).toBe(0)
   expect(stdout).toMatch(/1 test passed in \d+(\.\d+)?ms/)
