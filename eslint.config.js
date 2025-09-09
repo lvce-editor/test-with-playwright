56e731328731ae39ffa97833bbb2913981564f14
Le Vivilet <levivilet@pm.me>
2025-09-09 23:12:36 +0200

    feature: enable stricter eslint config (#311)

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,14 @@
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import * as config from '@lvce-editor/eslint-config'
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    ignores: [
-      'dist',
-      '.tmp',
-      '**/build/**',
-      '**/coverage/**',
-      '**/fixtures/**',
-      '**/e2e/**',
-      '**/test-with-playwright/**',
-      '**/test-with-playwright-worker/**',
-      'scripts',
-      'rollup.config.js',
-      'eslint.config.js',
-      'packages/text-search-worker/src/textSearchWorkerMain.ts',
-    ],
-  },
+export default [
+  ...config.default,
+  ...config.recommendedNode,
   {
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/consistent-type-exports': 'error',
-      '@typescript-eslint/consistent-type-imports': 'error',
+      'n/no-process-exit': 'off',
+      'unicorn/no-process-exit': 'off',
+      'no-console': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
     },
   },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      'no-case-declarations': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/restrict-template-expressions': 'off',
-    },
-  },
-)
+]
