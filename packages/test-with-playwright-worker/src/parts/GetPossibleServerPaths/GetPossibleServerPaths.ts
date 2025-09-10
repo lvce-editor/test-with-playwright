0be505d75299966a366d1e91f73f3251729244c6
Le Vivilet <levivilet@pm.me>
2025-09-10 20:34:51 +0200

    feature: hoist cwd

--- packages/test-with-playwright-worker/src/parts/GetPossibleServerPaths/GetPossibleServerPaths.ts
+++ packages/test-with-playwright-worker/src/parts/GetPossibleServerPaths/GetPossibleServerPaths.ts
@@ -1,15 +1,15 @@
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 
-export const getPossibleServerPaths = (): void => {
-  const toTry = [
+export const getPossibleServerPaths = (): readonly string[] => {
+  const cwd = process.cwd()
+  const toTry: readonly string[] = [
     '@lvce-editor/server',
-    pathToFileURL(join(process.cwd(), '..', 'server', 'node_modules', '@lvce-editor', 'server', 'index.js')).toString(),
-    pathToFileURL(join(process.cwd(), '..', 'build', 'node_modules', '@lvce-editor', 'server', 'index.js')).toString(),
+    pathToFileURL(join(cwd, '..', 'server', 'node_modules', '@lvce-editor', 'server', 'index.js')).toString(),
+    pathToFileURL(join(cwd, '..', 'build', 'node_modules', '@lvce-editor', 'server', 'index.js')).toString(),
     pathToFileURL(
-      join(process.cwd(), '..', '..', '..', 'packages', 'server', 'node_modules', '@lvce-editor', 'server', 'index.js'),
+      join(cwd, '..', '..', '..', 'packages', 'server', 'node_modules', '@lvce-editor', 'server', 'index.js'),
     ).toString(),
   ]
-  // @ts-ignore
   return toTry
 }
