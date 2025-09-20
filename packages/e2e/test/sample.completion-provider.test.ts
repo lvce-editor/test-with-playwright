2741f62f2dcd72f70b80971f49ab7bbfc28c2c62
Le Vivilet <levivilet@pm.me>
2025-09-20 23:46:21 +0200

    feature: use global jest timeout (#350)

--- packages/e2e/test/sample.completion-provider.test.ts
+++ packages/e2e/test/sample.completion-provider.test.ts
@@ -1,15 +1,9 @@
 import { test, expect } from '@jest/globals'
 import { runFixture } from '../src/_runFixture.ts'
 
-const timeout = 30_000
-
-test(
-  'sample.completion-provider',
-  async (): Promise<void> => {
-    const { stdout, exitCode } = await runFixture('sample.completion-provider')
-    expect(exitCode).toBe(0)
-    expect(stdout).toMatch(/1 test passed in \d+(\.\d+)?ms/)
-    // expect(stderr).toBe('')
-  },
-  timeout,
-)
+test('sample.completion-provider', async (): Promise<void> => {
+  const { stdout, exitCode } = await runFixture('sample.completion-provider')
+  expect(exitCode).toBe(0)
+  expect(stdout).toMatch(/1 test passed in \d+(\.\d+)?ms/)
+  // expect(stderr).toBe('')
+})
