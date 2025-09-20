1747e86f0052e36987440d26316695524149e9fb
Le Vivilet <levivilet@pm.me>
2025-09-20 23:42:05 +0200

    test

--- packages/e2e/test/sample.failing-test.test.ts
+++ packages/e2e/test/sample.failing-test.test.ts
@@ -4,5 +4,5 @@ import { runFixture } from '../src/_runFixture.ts'
 test('sample.failing-test', async (): Promise<void> => {
   const { stdout, exitCode } = await runFixture('sample.failing-test')
   expect(exitCode).toBe(1)
-  expect(stdout).toMatch(/1 test failed in \d+(\.\d+?)ms/)
+  expect(stdout).toMatch(/1 test failed in \d+ms/)
 })
