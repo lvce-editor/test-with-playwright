08db5b4b898a0f5324e0257d750e6a8f3c002689
Le Vivilet <levivilet@pm.me>
2025-11-25 15:48:56 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import * as config from '@lvce-editor/eslint-config'
+import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
   ...config.recommendedNode,
+  ...actions.default,
   {
     rules: {
       'n/no-process-exit': 'off',
