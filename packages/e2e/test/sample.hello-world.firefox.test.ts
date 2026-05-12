edc48105a706f4baa797e264aac345ca6656ae7c
Le Vivilet <levivilet@pm.me>
2026-05-12 13:33:59 +0000

    fix

--- packages/e2e/test/sample.hello-world.firefox.test.ts
+++ packages/e2e/test/sample.hello-world.firefox.test.ts
@@ -3,6 +3,8 @@ import { runFixture } from '../src/_runFixture.ts'
 
 const testPassedRegex = /1 test passed in \d+(\.\d+)?ms/
 
+export const skip = 1
+
 test('sample.hello-world in firefox', async (): Promise<void> => {
   const { exitCode, stdout } = await runFixture('sample.hello-world', ['--browser=firefox'])
   expect(exitCode).toBe(0)
