ae95b2eff7435a6564e763f3f652dabda2ca051d
Le Vivilet <levivilet@pm.me>
2026-06-03 16:31:58 +0200

    e2e

--- packages/e2e/test/sample.completion-provider.test.ts
+++ packages/e2e/test/sample.completion-provider.test.ts
@@ -3,7 +3,7 @@ import { runFixture } from '../src/_runFixture.ts'
 
 const testPassedRegex = /1 test passed in \d+(\.\d+)?ms/
 
-test('sample.completion-provider', async (): Promise<void> => {
+test.skip('sample.completion-provider', async (): Promise<void> => {
   const { exitCode, stdout } = await runFixture('sample.completion-provider')
   expect(exitCode).toBe(0)
   expect(stdout).toMatch(testPassedRegex)
