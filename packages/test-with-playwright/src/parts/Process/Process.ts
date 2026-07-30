04d598a12ad332bd0fde1c94f9d71944b1dc6c47
Le Vivilet <levivilet@pm.me>
2026-07-30 19:47:58 +0000

    feature: add knip

--- packages/test-with-playwright/src/parts/Process/Process.ts
+++ packages/test-with-playwright/src/parts/Process/Process.ts
@@ -2,10 +2,6 @@ export const { argv } = process
 
 export const { env } = process
 
-export const exit = (code: number): void => {
-  process.exit(code)
-}
-
 export const on = (event: string, listener: (...args: any[]) => void): void => {
   process.on(event, listener)
 }
