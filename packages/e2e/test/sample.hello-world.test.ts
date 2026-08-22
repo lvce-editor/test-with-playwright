9b9fd49bee8ed5fc0fe835028ad9303c2f9ec71a
Le Vivilet <levivilet@pm.me>
2026-08-22 08:17:06 +0000

    fix: capture flaky renderer messages

--- packages/e2e/test/sample.hello-world.test.ts
+++ packages/e2e/test/sample.hello-world.test.ts
@@ -4,7 +4,7 @@ import { runFixture } from '../src/_runFixture.ts'
 const testPassedRegex = /1 test passed in \d+(\.\d+)?ms/
 
 test('sample.hello-world', async (): Promise<void> => {
-  const { exitCode, stdout } = await runFixture('sample.hello-world')
+  const { exitCode, stdout } = await runFixture('sample.hello-world', ['--trace-renderer-worker'])
   expect(exitCode).toBe(0)
   expect(stdout).toMatch(testPassedRegex)
   // expect(stderr).toBe('')
